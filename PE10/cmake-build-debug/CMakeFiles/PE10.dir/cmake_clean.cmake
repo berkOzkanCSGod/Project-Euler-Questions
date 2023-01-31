8aaabf896a528a99c8e24232d21fbb89445b740b
Berk Özkan <82842011+berkOzkanCSGod@users.noreply.github.com>
2023-01-31 21:01:00 +0300

    Add files via upload
    PE10

---
+++ PE10/cmake-build-debug/CMakeFiles/PE10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PE10.dir/main.cpp.o"
+  "PE10"
+  "PE10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PE10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
